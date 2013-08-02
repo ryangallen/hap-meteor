6db79cb53edf912e537a3507f06b9c0bcbf4771d
Ryan Allen <ryan@ryangallen.com>
2013-08-02 02:58:27 -0400

    fix month names and add search box to header

--- client/views/pins/pin_item.js
+++ client/views/pins/pin_item.js
@@ -3,5 +3,27 @@ Template.pinItem.helpers({
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
+	},
+
+	date: function(){
+		d = new Date(this.date + ' ' + this.time);
+		var monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+		var month = d.getMonth(),
+			day = d.getDate(),
+			year = d.getFullYear();
+		return monthNames[month] + ' ' + day + ', ' + year;
+	},
+
+	time: function(){
+		d = new Date(this.date + ' ' + this.time);
+		var hour = d.getHours(),
+			min = d.getMinutes(),
+			period = 'am';
+		if (hour > 12){hour = hour%12;period = 'pm';}
+		return hour + ':' + pad(min) + ' ' + period;
 	}
 });
+
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
